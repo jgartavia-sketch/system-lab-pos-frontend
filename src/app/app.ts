6f571f2b60632f377228c53cdcaf298e6ce9d731
TU_NOMBRE <jgartavia@gmail.com>
2026-08-04 18:00:37 -0600

    Sube frontend inicial de System Lab POS

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('systemlab-pos-front');
+  protected readonly isSidebarOpen = signal(false);
+
+  protected toggleSidebar(): void {
+    this.isSidebarOpen.update((currentValue) => !currentValue);
+  }
+
+  protected closeSidebar(): void {
+    this.isSidebarOpen.set(false);
+  }
 }
