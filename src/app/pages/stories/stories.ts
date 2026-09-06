5d99b91b3bfe4c8a527002a43cdcadb5d4b32c6c
Jose Artavia <jgartavia@gmail.com>
2026-09-05 23:06:52 -0600

    Convierte personajes de Stories en acordeones compactos

--- src/app/pages/stories/stories.ts
+++ src/app/pages/stories/stories.ts
@@ -9,10 +9,20 @@ import { RouterLink } from '@angular/router';
 })
 export class Stories {
   readonly chapters = [1, 2, 3, 4, 5, 6];
-  readonly characterSlots = [1, 2, 3, 4];
+  readonly characters = [
+    { archive: '01', name: 'Tripulante 1', image: '', role: 'Por revelar', origin: 'Clasificado', mission: 'Información reservada' },
+    { archive: '02', name: 'Tripulante 2', image: '', role: 'Por revelar', origin: 'Clasificado', mission: 'Información reservada' },
+    { archive: '03', name: 'Tripulante 3', image: '', role: 'Por revelar', origin: 'Clasificado', mission: 'Información reservada' },
+    { archive: '04', name: 'Tripulante 4', image: '', role: 'Por revelar', origin: 'Clasificado', mission: 'Información reservada' },
+  ];
   selectedChapter = 1;
+  expandedCharacter: number | null = null;
 
   selectChapter(chapter: number): void {
     this.selectedChapter = chapter;
+  }
+
+  toggleCharacter(index: number): void {
+    this.expandedCharacter = this.expandedCharacter === index ? null : index;
   }
 }
